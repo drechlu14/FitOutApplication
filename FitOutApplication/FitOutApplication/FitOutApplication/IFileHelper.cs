f7740a98cb2538a3aba154151d07174a150879bf
Mirak52 <marek.bastl5@gmail.com>
2017-05-23 21:53:05 +0200

    Databáze

---
+++ FitOutApplication/FitOutApplication/FitOutApplication/IFileHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitOutApplication
+{
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+}
